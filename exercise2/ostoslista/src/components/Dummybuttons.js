14d4876d74b10a6b5edddabc76a095ed48a02be9
maijacita <92377463+maijacita@users.noreply.github.com>
2022-02-24 10:23:46 +0200

    Add files via upload

---
+++ exercise2/ostoslista/src/components/Dummybuttons.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function Dummybuttons(props) {
+  return <div>
+  <button className="itemButtons" onClick={ ()=> props.dummyMe(1, "Beer",1) } >Add Beer</button>
+  <button className="itemButtons" onClick={ ()=> props.dummyMe(2, "Vine", 1) }>Add Vine</button>
+  <button className="itemButtons" onClick={ ()=> props.dummyMe(3, "Liquor", 1) }>Add Liquor</button>
+  <button className="itemButtons" onClick={ ()=> props.dummyMe(4, "Punssi", 1) }>Add kossu</button>
+</div>;
+}
